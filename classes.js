23fb1774e04294b797aff180dad074580b0d9cd1
ponmanivasahan <vasupks01@gmail.com>
2026-04-05 09:57:15 +0530

    removed the boundraies highlight colors

--- classes.js
+++ classes.js
@@ -1,51 +1,271 @@
-class Sprite{
-    constructor({position,velocity,image,frames={max:1}, Sprites}){
-        this.position=position;
-        this.image=image;
-        this.frames={...frames,val:0,elapsed:0};
-
-        this.image.onload=()=>{
-            this.width=this.image.width/this.frames.max;
-            this.height=this.image.height;
+class Sprite {
+    constructor({
+        position,
+        velocity,
+        image,
+        frames = { max: 1, hold: 10 },
+        sprites,
+        animate = false,
+        rotation = 0,
+        scale = 1
+    }) {
+        this.position = position;
+        this.velocity = velocity;
+        this.image = new Image();
+        this.frames = { ...frames, val: 0, elapsed: 0 };
+        this.image.onload = () => {
+            this.width = (this.image.width / this.frames.max) * scale;
+            this.height = this.image.height * scale;
         };
-        this.moving=false;
-        this.sprites=Sprites;
+        this.image.src = image.src;
+        this.animate = animate;
+        this.sprites = sprites;
+        this.opacity = 1;
+        this.rotation = rotation;
+        this.scale = scale;
     }
-    draw(){
+
+    draw() {
         if (!this.image.complete || this.image.naturalWidth === 0) return;
+
+        c.save();
+        c.translate(
+            this.position.x + this.width / 2,
+            this.position.y + this.height / 2
+        );
+        c.rotate(this.rotation);
+        c.translate(
+            -this.position.x - this.width / 2,
+            -this.position.y - this.height / 2
+        );
+        c.globalAlpha = this.opacity;
+
+        const crop = {
+            position: {
+                x: this.frames.val * (this.width / this.scale),
+                y: 0
+            },
+            width: this.image.width / this.frames.max,
+            height: this.image.height
+        };
+
+        const image = {
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            width: this.image.width / this.frames.max,
+            height: this.image.height
+        };
 
         c.drawImage(
             this.image,
-            this.frames.val * (this.image.width / this.frames.max),
-            0,
-            this.image.width/this.frames.max,
-            this.image.height,this.position.x,this.position.y,
-            this.image.width/this.frames.max,
-            this.image.height,
+            crop.position.x,
+            crop.position.y,
+            crop.width,
+            crop.height,
+            image.position.x,
+            image.position.y,
+            image.width * this.scale,
+            image.height * this.scale
         );
 
-        if(this.moving){
-            if(this.frames.max>1){
-                this.frames.elapsed++;
+        c.restore();
+
+        if (!this.animate) return;
+        if (this.frames.max > 1) {
+            this.frames.elapsed++;
+        }
+        if (this.frames.elapsed % this.frames.hold === 0) {
+            if (this.frames.val < this.frames.max - 1) this.frames.val++;
+            else this.frames.val = 0;
+        }
+    }
+}
+
+class Monster extends Sprite {
+    constructor({
+        position,
+        velocity,
+        image,
+        frames = { max: 1, hold: 10 },
+        sprites,
+        animate = false,
+        rotation = 0,
+        isEnemy = false,
+        name,
+        attacks
+    }) {
+        super({
+            position,
+            velocity,
+            image,
+            frames,
+            sprites,
+            animate,
+            rotation
+        });
+        this.health = 100;
+        this.isEnemy = isEnemy;
+        this.name = name;
+        this.attacks = attacks;
+    }
+
+    faint() {
+        document.querySelector('#dialogueBox').innerHTML = `${this.name} fainted!`;
+        gsap.to(this.position, {
+            y: this.position.y + 20
+        });
+        gsap.to(this, {
+            opacity: 0
+        });
+
+        if (typeof audio !== 'undefined') {
+            audio.battle?.stop?.();
+            audio.victory?.play?.();
+        }
+    }
+
+    attack({ attack, recipient, renderedSprites }) {
+        document.querySelector('#dialogueBox').style.display = 'block';
+        document.querySelector('#dialogueBox').innerHTML = `${this.name} used ${attack.name}`;
+
+        let healthBar = '#enemyHealthBar';
+        if (this.isEnemy) healthBar = '#playerHealthBar';
+
+        let rotation = 1;
+        if (this.isEnemy) rotation = -2.2;
+
+        recipient.health -= attack.damage;
+
+        switch (attack.name) {
+            case 'Fireball': {
+                if (typeof audio !== 'undefined') audio.initFireball?.play?.();
+
+                const fireballImage = new Image();
+                fireballImage.src = './images/fireball.png';
+                const fireball = new Sprite({
+                    position: {
+                        x: this.position.x,
+                        y: this.position.y
+                    },
+                    image: fireballImage,
+                    frames: {
+                        max: 4,
+                        hold: 10
+                    },
+                    animate: true,
+                    rotation
+                });
+
+                renderedSprites.splice(1, 0, fireball);
+
+                gsap.to(fireball.position, {
+                    x: recipient.position.x,
+                    y: recipient.position.y,
+                    onComplete: () => {
+                        if (typeof audio !== 'undefined') audio.fireballHit?.play?.();
+                        gsap.to(healthBar, {
+                            width: `${recipient.health}%`
+                        });
+
+                        gsap.to(recipient.position, {
+                            x: recipient.position.x + 10,
+                            yoyo: true,
+                            repeat: 5,
+                            duration: 0.08
+                        });
+
+                        gsap.to(recipient, {
+                            opacity: 0,
+                            repeat: 5,
+                            yoyo: true,
+                            duration: 0.08
+                        });
+
+                        renderedSprites.splice(1, 1);
+                    }
+                });
+                break;
             }
-            if(this.frames.elapsed%10===0){
-                if(this.frames.val<this.frames.max-1) this.frames.val++;
-                else this.frames.val=0;
+            case 'Tackle': {
+                const t1 = gsap.timeline();
+
+                let movementDistance = 20;
+                if (this.isEnemy) movementDistance = -20;
+
+                t1.to(this.position, {
+                    x: this.position.x - movementDistance
+                }).to(this.position, {
+                    x: this.position.x + movementDistance * 2,
+                    duration: 0.1,
+                    onComplete: () => {
+                        if (typeof audio !== 'undefined') audio.tackleHit?.play?.();
+                        gsap.to(healthBar, {
+                            width: `${recipient.health}%`
+                        });
+
+                        gsap.to(recipient.position, {
+                            x: recipient.position.x + 10,
+                            yoyo: true,
+                            repeat: 5,
+                            duration: 0.08
+                        });
+
+                        gsap.to(recipient, {
+                            opacity: 0,
+                            repeat: 5,
+                            yoyo: true,
+                            duration: 0.08
+                        });
+                    }
+                }).to(this.position, {
+                    x: this.position.x
+                });
+                break;
             }
         }
     }
 }
 
-class Boundary{
-    static width=48;
-    static height=48;
-        constructor({position}){
-            this.position=position;
-      this.width=48;
-      this.height=48;
+class Boundary {
+    static width = 48;
+    static height = 48;
+
+    constructor({ position }) {
+        this.position = position;
+        this.width = 48;
+        this.height = 48;
     }
-    draw(){
-        c.fillStyle='rgba(225,0,0,0.5)';
-        c.fillRect(this.position.x,this.position.y,this.width,this.height);
+
+    draw() {
+        // Collision boundaries are invisible in gameplay.
+    }
+}
+
+class Character extends Sprite {
+    constructor({
+        position,
+        velocity,
+        image,
+        frames = { max: 1, hold: 10 },
+        sprites,
+        animate = false,
+        rotation = 0,
+        scale = 1,
+        dialogue = ['']
+    }) {
+        super({
+            position,
+            velocity,
+            image,
+            frames,
+            sprites,
+            animate,
+            rotation,
+            scale
+        });
+        this.dialogue = dialogue;
+        this.dialogueIndex = 0;
     }
 }
